70d28ff1b482e7debed6205a1026c58af0f48791
HanninenF <hanninenfredrik@gmail.com>
2025-03-11 13:24:48 +0100

    add: css and html for MonsterForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 ﻿import "./App.scss";
 
 function App() {
-  return <></>;
+  /* form */
+  function MonsterForm() {
+    return (
+      <>
+        <div className="monsterForm__container">
+          <form className="monsterForm" action="">
+            <fieldset className="monsterFieldSet">
+              <legend>Lägg till student</legend>
+              <label htmlFor="">
+                <input className="monsterName__input" type="text" />
+              </label>
+              <label htmlFor="">
+                <input className="monsterSurname__input" type="text" />
+              </label>
+              <label htmlFor="">
+                <input className="monsterCourse" type="text" />
+              </label>
+              <label htmlFor="">
+                <input className="monsterAge" type="text" />
+              </label>
+            </fieldset>
+          </form>
+        </div>
+      </>
+    );
+  }
+
+  {
+  }
+  return (
+    <>
+      <MonsterForm />
+    </>
+  );
 }
 
 export default App;
