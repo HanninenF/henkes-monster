285bef4436bd38e147009bf23a414aba1ea41989
HanninenF <hanninenfredrik@gmail.com>
2025-03-11 14:17:47 +0100

    refactor: MonsterForm out of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 ﻿import { useEffect, useState } from "react";
 import "./App.scss";
 
-function App() {
+type MonsterForm = {
+  nameInput: string;
+};
+
+/* form */
+function MonsterForm() {
   //inputState
   const [nameInput, setNameInput] = useState<string>("");
 
@@ -13,38 +18,37 @@ function App() {
   useEffect(() => {
     console.log("nameInput", nameInput);
   }, [nameInput]);
-  /* form */
-  function MonsterForm() {
-    return (
-      <>
-        <div className="monsterForm__container">
-          <form className="monsterForm" action="">
-            <fieldset className="monsterFieldSet">
-              <legend>Lägg till student</legend>
-              <label htmlFor="">
-                <input
-                  className="monsterName__input"
-                  type="text"
-                  value={nameInput}
-                  onChange={handleNameInput}
-                />
-              </label>
-              <label htmlFor="">
-                <input className="monsterSurname__input" type="text" />
-              </label>
-              <label htmlFor="">
-                <input className="monsterCourse" type="text" />
-              </label>
-              <label htmlFor="">
-                <input className="monsterAge" type="text" />
-              </label>
-            </fieldset>
-          </form>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className="monsterForm__container">
+        <form className="monsterForm" action="">
+          <fieldset className="monsterFieldSet">
+            <legend>Lägg till student</legend>
+            <label htmlFor="">
+              <input
+                className="monsterName__input"
+                type="text"
+                value={nameInput}
+                onChange={handleNameInput}
+              />
+            </label>
+            <label htmlFor="">
+              <input className="monsterSurname__input" type="text" />
+            </label>
+            <label htmlFor="">
+              <input className="monsterCourse" type="text" />
+            </label>
+            <label htmlFor="">
+              <input className="monsterAge" type="text" />
+            </label>
+          </fieldset>
+        </form>
+      </div>
+    </>
+  );
+}
 
+function App() {
   {
   }
   return (
